086d5ab5302ba44e00172691f298f820b0655e0f
William Zimmermann <wzimmermann@LGQCSUP020.cogismaq.com>
2021-01-06 16:17:17 -0500

    Something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, Inject } from '@angular/core';
+import { ButtonComponent } from '@syncfusion/ej2-angular-buttons';
+
 // WZ - it is necessary to import jss heree
 import jss, { StyleSheet } from 'jss';
-import {styles, red, green, blue} from './app.component.styles';
+import {red, green, blue} from './app.component.styles';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent{
+  @ViewChild('toggleBtn')
+  public toggleBtn: ButtonComponent;
+
   public mySheet: StyleSheet;
   public title = 'angular-jss';
 
-  constructor(){
-    this.mySheet = jss.createStyleSheet(styles, {link: true}).attach();
-  }
-  public changeColor(color: string): void{
-    console.log("Chamou");
-    var finalColor: string;
+  constructor(@Inject('sourceFiles') private sourceFiles: any){
+    sourceFiles.files = ['button.css'];
 
-    switch(color){
-      case 'red':
-        finalColor = red;
-        break;
-      case 'blue':
-        finalColor = blue;
-        break;
-      default:
-        finalColor = green;
-        break;
-    }
+    // CSS IN JS
+    this.mySheet = jss.createStyleSheet({
+      title: {
+        textAlign: 'center',
+        backgroundColor: '#E0E0E0',
+        '&:hover': {
+            backgroundColor: '#BDBDBD'
+        }
+      },
+      area: {
+          width: '100%',
+          height: '10rem',
+          color: 'white',
+          backgroundColor: (data: any) => data.area.backgroundColor
+      }
+    }, {link: true}).attach();
+  }
+  
+  public changeColor(color: string): void{
 
     this.mySheet.update({
-      area: {backgroundColor: finalColor}
+      area: {backgroundColor: red}
     });
   }
 }
